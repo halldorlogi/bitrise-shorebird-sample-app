eae4a0a324d4ae9655f7be73b57c4250d1dfb18e
Jon Holtan <jon@uptechstudio.com>
2023-09-26 10:23:02 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# shorebird_sample_app
+
+A sample app that is used to test the Bitrise Shorebird Release & Patch steps.
---
+++ android/app/src/main/kotlin/com/example/shorebird_sample_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shorebird_sample_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
